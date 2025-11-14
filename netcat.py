bee531d8256d1d71039135800da41d963c29f78a
17ev <ndevore04@gmail.com>
2025-11-14 16:58:14 -0600

    fixed run() function

--- netcat.py
+++ netcat.py
@@ -5,11 +5,6 @@ import subprocess
 import sys
 import textwrap
 import threading
-from pyfiglet import Figlet
-
-def displayBanner():
-    f = Figlet(font='isometric1')
-    print(f.renderText('Gu\'s NetCAT'))
 
 def execute(cmd):
     cmd = cmd.strip()
@@ -26,87 +21,87 @@ class NetCat:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        def run(self):
+    def run(self):
             if self.args.listen:
                 self.listen()
             else:
                 self.send()
         
-        def send(self):
-            self.socket.connect((self.args.target, self.args.port)) #1
-            if self.buffer:
-                self.socket.send(self.buffer)
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port)) #1
+        if self.buffer:
+            self.socket.send(self.buffer)
             
-            try: #2
-                while True:
-                    recv_len = 1
-                    response = ''
-                    while recv_len:
-                        data = self.socket.recv(4096)
-                        recv_len = len(data)
-                        response += data.decode()
-                        if recv_len < 4096:
-                            break
-                    if response:
-                        print(response)
-                        buffer = input('> ')
-                        buffer += '\n'
-                        self.socket.send(buffer.endcode())
-            except KeyboardInterrupt:
-                print('User terminated.')
-                self.socket.close()
-                sys.exit()
-
-        def listen(self):
-            self.socket.bind((self.args.target, self.args.port))
-            self.socket.listen(5)
-
+        try: #2
             while True:
-                client_socket, _ = self.socket.accept()
-                client_thread = threading.Thread(
-                    target = self.handle, args=(client_socket,)
-                )
-                client_thread.start()
-        
-        def handle(self, client_socket):
-            if self.args.execute:
-                output = execute(self.args.execute)
-                client_socket.send(output.encode())
-            
-            elif self.args.upload:
-                file_buffer = b''
-                while True:
-                    data = client_socket.recv(4096)
-                    if data:
-                        file_buffer += data
-                    else:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
                         break
-                with open(self.args.upload, 'wb') as f:
-                    f.write(file_buffer)
-                message = f'Saved file {self.args.upload}'
-                client_socket.send(message.encode())
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.endcode())
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target = self.handle, args=(client_socket,)
+            )
+            client_thread.start()
+        
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
             
-            elif self.args.commmand:
-                cmd_buffer = b''
-                while True:
-                    try:
-                        client_socket.send(b'BHP: #> ')
-                        while '\n' not in cmd_buffer.decode():
-                            cmd_buffer += client_socket.recv(64)
-                        response = execute(cmd_buffer.decode())
-                        if response:
-                            client_socket.send(response.endcode())
-                        cmd_buffer = b''
-                    except Exception as e:
-                        print(f'server killed {e}')
-                        self.socket.close()
-                        sys.exit()
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+            message = f'Saved file {self.args.upload}'
+            lient_socket.send(message.encode())
+            
+        elif self.args.commmand:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b'BHP: #> ')
+                    while '\n' not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(64)
+                    response = execute(cmd_buffer.decode())
+                    if response:
+                        client_socket.send(response.endcode())
+                    cmd_buffer = b''
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close()
+                    sys.exit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description= 'BHP Net Tool',
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilop=textwrap.dedent9('''Example:
+        epilog=textwrap.dedent('''Example:
             netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
             netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
             netcat.py -t 192.168.1.108 -p 5555 -l -e\"cat /etc/passwd\" # execute command
@@ -121,13 +116,12 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
     parser.add_argument('-u', '--upload', help='upload file')
     args = parser.parse_args()
-    if arg.listen:
+    if args.listen:
         buffer = ''
     else:
         buffer = sys.stdin.read()
     
     nc = NetCat(args, buffer.encode())
-    nc.run
-
+    nc.run()
 
     
